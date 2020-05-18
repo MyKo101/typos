bc4b419057015e847f3fbf0a4fe26e955a2ea9f3
MyKo101 <48382869+MyKo101@users.noreply.github.com>
2020-05-18 23:53:15 +0100

    Add MakeTypos

---
+++ R/MakeTypos.R
@@ -0,0 +1,22 @@
+
+MakeTypos <- function()
+{
+  #Load utils
+  source("R/utils.R")
+  cat("\nutils functions loaded")
+
+  #Create the exported typos
+  .clear_exported()
+  cat("\nExported function file cleared")
+
+  cat("\nLoading in the current list of typos")
+  #Add in the current list of typos
+  .typo_doc(nameS,names)
+  .typo_doc(`nameS<-`,`names<-`,back.tick=T)
+  .typo_doc(typoef,typeof)
+  .typo_doc(geom_warp,geom_wrap,ggplot2)
+  .typo_doc(fitler,filter,dplyr)
+  .typo_doc(transmutate,mutate,dplyr)
+  .typo_doc(lenght,length)
+
+}
